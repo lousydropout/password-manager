649dac61f4c66f47e7eb48076ce67b34bce06766
lousydropout <lousydropout@gmail.com>
2024-02-17 01:54:38 -0800

    move ConnectButton to Header

---
+++ frontend/src/components/layout/Header.tsx
@@ -0,0 +1,26 @@
+import { ConnectButton } from '@/components/web3/ConnectButton'
+import { HStack } from '@chakra-ui/react'
+import Image from 'next/image'
+import Link from 'next/link'
+import inkathonLogo from 'public/brand/inkathon-logo.png'
+import 'twin.macro'
+
+export const Header = () => {
+  const title = 'KeyVault'
+  const githubHref = 'https://github.com/lousydropout/password-manager'
+  return (
+    <HStack tw="flex justify-between px-8 py-4">
+      {/* Logo & Title */}
+      <Link
+        href={githubHref}
+        target="_blank"
+        className="group"
+        tw="flex cursor-pointer gap-4 rounded-3xl py-1.5 px-3.5 transition-all hover:bg-gray-900"
+      >
+        <Image src={inkathonLogo} priority width={60} alt="ink!athon Logo" tw="inline" />
+        <h1 tw="inline font-black text-[2.5rem]">{title}</h1>
+      </Link>
+      <ConnectButton tw="inline" />
+    </HStack>
+  )
+}
